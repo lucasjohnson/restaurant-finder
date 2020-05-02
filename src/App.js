9c664ccebe2e7de3b2d782a68a8b84474dfda7ca
Lucas Johnson <contact@lucasjohnson.co.nz>
2020-05-02 14:58:30 -0400

    created basic routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
+import LandingPage from './pages/LandingPage'
 
 const App = () => {
-  return <h1>Food App</h1>
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={LandingPage} />
+        <Redirect to="/" />
+      </Switch>
+    </Router>
+  )
 }
 
 export default App
