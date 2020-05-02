e9cd61a9f04d56b6ef5b2a6f4fe9af05e3154352
Lucas Johnson <contact@lucasjohnson.co.nz>
2020-05-02 14:59:29 -0400

    created reducers for api call

--- src/reducers/restaurantsReducer.js
+++ src/reducers/restaurantsReducer.js
@@ -1,3 +1,5 @@
+import * as actions from '../actions/restaurantsActions'
+
 export const initialState = {
   restaurants: [],
   loading: false,
@@ -6,6 +8,12 @@ export const initialState = {
 
 export default function restaurantsReducer(state = initialState, action) {
   switch (action.type) {
+    case actions.GET_RESTAURANTS:
+      return { ...state, loading: true }
+    case actions.GET_RESTAURANTS_SUCCESS:
+      return { restaurants: action.payload, loading: false, hasErrors: false }
+    case actions.GET_RESTAURANTS_FAILURE:
+      return { ...state, loading: false, hasErrors: true }
     default:
       return state
   }
