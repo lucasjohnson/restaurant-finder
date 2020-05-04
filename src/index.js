1b0ea08a7f660b940778935ffbfbfb22b3dd2c80
Lucas Johnson <contact@lucasjohnson.co.nz>
2020-05-04 00:25:50 -0400

    reomved polyfills

--- src/index.js
+++ src/index.js
@@ -7,26 +7,17 @@ import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import App from './App'
 import rootReducer from './reducers'
-import { polyfillLoader } from 'polyfill-io-feature-detection'
-import { polyfill } from 'es6-promise'; polyfill()
-
-polyfillLoader({
-  "features": "Promise,fetch",
-  "onCompleted": main
-});
 
 const store = createStore(
   rootReducer,
   composeWithDevTools(applyMiddleware(thunk))
 )
 
-function main() {
-  render (
-    <Provider store={store}>
-      <App />
-    </Provider>,
-    document.getElementById('root')
-  )
-}
+render (
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
 
 serviceWorker.register();
