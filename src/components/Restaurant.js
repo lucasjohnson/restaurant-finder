5316562657d6175100a4585deef0f89db4715673
Lucas Johnson <contact@lucasjohnson.co.nz>
2020-05-04 10:57:51 -0400

    added prices icon depending on value of returned number

--- src/components/Restaurant.js
+++ src/components/Restaurant.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { isMobile } from '../helpers/helpers'
+import { FaDollarSign } from 'react-icons/fa';
 import './Restaurant.scss'
 
 export default class Restaurant extends Component {
@@ -25,16 +26,25 @@ export default class Restaurant extends Component {
     window.removeEventListener('resize', this.setMobileState);
   }
 
+  renderPrice = (price) => {
+    return (
+      Array.apply(null, { length: price }).map((element, index) => (
+        <FaDollarSign key={index} />
+      ))
+    )
+  }
+
   render() {
     const { restaurant } = this.props
     const { isMobile } = this.state
 
     return (
       <li className="restaurant-item">
+        <span className="restaurant-price">
+          {this.renderPrice(restaurant.price)}
+        </span>
         <img className="restaurant-image" src={restaurant.image_url} alt={restaurant.name} />
-        <h2 className="restaurant-title">
-          {restaurant.name}
-        </h2>
+        <h2 className="restaurant-title">{restaurant.name}</h2>
         <p className="restaurant-address">{`${restaurant.address}, ${restaurant.postal_code}`}</p>
         <p className="restaurant-phone">{restaurant.phone}</p>
         <a
